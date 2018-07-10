319cd2a4d677b89adcdb533d8ea7c0c116b5e91a
Adams1991 <adams121@hotmail.co.uk>
2018-07-10 15:57:54 +0100

    file model setup complete

--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,4 +1,50 @@
 package models;
 
+import javax.persistence.*;
+import java.util.List;
+
 public class Folder {
+
+    private int id;
+    private String title;
+    private List<File> files;
+
+    public Folder(String title, List<File> files){
+        this.title = title;
+        this.files = files;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+
 }
+
+
+// A folder should have a title and list of files.
