45342de84e31cf51cb9903ca7a77cc1a04e36dcb
Adams1991 <adams121@hotmail.co.uk>
2018-07-10 16:20:12 +0100

    delete, update and save working but issue with getall and find

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,10 +11,23 @@ public class Runner {
         Folder folder = new Folder("Downloads");
         DBHelper.save(folder);
 
+        Folder folder2 = new Folder("Downloads");
+        DBHelper.save(folder2);
+
+
         File file1 = new File("Bla", ".rb", 30, folder);
         DBHelper.save(file1);
 
+        File file2 = new File("Bla", ".rb", 30, folder);
+        DBHelper.delete(file2);
 
+        List<File> files = DBHelper.getAll(File.class);
+
+        File foundFile = DBHelper.find(File.class, file1.getId());
+
+
+        file1.setName("bababa");
+        DBHelper.update(file1);
 
 
     }
